90d1cf20a922a2e3127f8828c99d4fea2e1e12f7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-25 12:43:06 -0600

    Positioned the nav bars and body using left and right properties based on a 3.5rem width and height

--- shard-mock-up/src/App.js
+++ shard-mock-up/src/App.js
@@ -3,24 +3,36 @@ import './App.css'
 
 function App() {
   return (
-    <div className="container-fluid">
+    <div>
 
-      <div className="row top">
+      <div className="top">
         <p className="my-2"> Settings </p>
-        <a className="btn btn-primary my-2 logout" href="/" role="button"> Log Out </a>
+        <a className="btn btn-primary logout" href="/" role="button"> Log Out </a>
       </div>
 
-      <div className="row">
-        <div className="col-2 leftNav"> Left Nav
-        {/* <a href="#" rel="noopener noreferrer"/> */}
+      <div className="leftNav">
+        <div className="sideNavItem">
           <i className="fas fa-search"> </i>
         </div>
-
-        <div className="row body">
-          <div className="col-4 bodyLeft"> Left Body </div>
-          <div className="col-8 bodyRight"> Right Body </div>
-        </div>
+        {/* <a href="#" rel="noopener noreferrer"/> */}
       </div>
+
+
+
+
+      <div className="mainBody">
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4 bodyLeft"> Left Body </div>
+            <div className="col-8 bodyRight"> Right Body </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
 
     </div>
   )
