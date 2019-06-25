8d26b0044fa352a76dc655a2fb2d3cf30be7a9ab
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-25 14:25:55 -0600

    Added all icons to leftNav

--- shard-mock-up/src/App.js
+++ shard-mock-up/src/App.js
@@ -11,24 +11,26 @@ function App() {
       </div>
 
       <div className="leftNav">
-
         <div className="sideNavSearch">
           <i className="fas fa-search fa-lg icon"> </i>
         </div>
 
         <div className="sideNavItem">
-          <i className="fas fa-search fa-lg icon"> </i>
+          <i className="fab fa-delicious fa-lg icon"> </i>
+        </div>
+        
+        <div className="sideNavItem">
+          <i className="fas fa-bars fa-lg icon"> </i>
         </div>
         {/* <a href="#" rel="noopener noreferrer"/> */}
 
         <div className="sideNavItem">
-          <i className="fas fa-bell fa-lg icon"> </i>
+          <i className="fas fa-user-circle fa-lg icon"> </i>
         </div>
 
         <div className="sideNavItem">
-          <i className="fas fa-user-circle fa-lg icon"> </i>
+          <i className="fas fa-bell fa-lg icon"> </i>
         </div>
-        
       </div>
       
       <div className="mainBody">
