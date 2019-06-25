6c57cd1e2e3c2bf797e507c9db13603dfd678d6c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-24 19:35:17 -0600

    Add a search icon

--- shard-mock-up/src/App.js
+++ shard-mock-up/src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import './App.css';
-
-{/* <a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
-> */}
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="container-fluid">
 
-      <div className="row top"> 
+      <div className="row top">
         <p classname="my-2"> Settings </p>
         <a className="btn btn-primary my-2 logout" href="#" role="button"> Log Out </a>
       </div>
 
-      <div className="col-1 leftNav">Left Nav</div>
-     
-      <div className="body">Body</div>
+      <div className="col-2 leftNav"> Left Nav
+        {/* <a href="#" rel="noopener noreferrer"/> */}
+        <i className="fas fa-search"> </i> 
+      </div>
 
-    </div>
-  );
-}
+      <div className="body"> Body </div>
+    
+      </div>
 
-export default App;
+      )
+    }
+    
+    export default App
