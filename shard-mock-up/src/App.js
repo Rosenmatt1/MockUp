58d0dcfa9b4af24d5ccf054541a853477e8764d7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-25 15:48:57 -0600

    Added classes for flexbox styling of the top nav elements and changed the body color to a grey

--- shard-mock-up/src/App.js
+++ shard-mock-up/src/App.js
@@ -6,24 +6,31 @@ function App() {
   return (
     <div>
 
-      <div className="top">
-        <p className="my-2"> Settings </p>
-        <a className="btn btn-primary logout" href="/" role="button"> Log Out </a>
+      <div className="row top">
+
+        <div className="setting">
+          <p className="my-auto"> Settings </p>
+        </div>
+
+        <div className="logout">
+          <a className="btn btn-primary" href="/" role="button"> Log Out </a>
+        </div>
+
       </div>
 
-      <LeftNav/>
-      
-      <div className="mainBody">
 
-        <div className="container-fluid">
+      <LeftNav />
+
+
+      <div className="container-fluid mainBody">
+        {/* <div className="container-fluid"> */}
           <div className="row">
             <div className="col-4 bodyLeft"> Left Body </div>
             <div className="col-8 bodyRight"> Right Body </div>
           </div>
-        </div>
-
-
+        {/* </div> */}
       </div>
+
     </div>
   )
 }
