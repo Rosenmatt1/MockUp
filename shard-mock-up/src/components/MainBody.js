c96eafa618417807b1ee86e886e7140af08fa4b0
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-27 00:32:30 -0600

    Got margins and padding to be pretty much exactly where I want it to be for settings component

--- shard-mock-up/src/components/MainBody.js
+++ shard-mock-up/src/components/MainBody.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainBody = (props) => {
   return (
-    < div className="container-fluid mainBody" >
+    <div className="container-fluid mainBody">
       <div className="row">
 
         <div className="col-4 bodyLeft">
@@ -14,17 +14,17 @@ const MainBody = (props) => {
 
         <div className="col-8 bodyRight">
           <div className="rightContent">
-            <div className="textBox my-2"><p> General Settings </p></div>
+            <div className="title my-2"><p> General Settings </p></div>
             <hr className="hrBoxes" />
 
             <div className="row">
               <div>
-                <div className="textBox my-4"> Email </div>
+                <div className="textBox mb-4"> Email </div>
                 <div className="textBox my-4"> Commission Charges </div>
                 <div className="textBox"> Tracking Expense </div>
               </div>
               <div className="ml-4 rightSetting">
-                <div className="textBox my-4"> <input className="inputStyle1" placeholder="gregory.murynmukha@gmail.com" /> </div>
+                <div className="textBox mb-4"> <input className="inputStyle1" placeholder="gregory.murynmukha@gmail.com" /> </div>
                 <div className="textBox my-4"><input className="inputStyle" type="checkbox" />  Take into account Apple commision charges (30%) </div>
                 <div className="textBox"> <input className="inputStyle" placeholder="$0.00" /> per each install </div>
               </div>
