ac4f1891ebe6991eadfc9f343e275cd426c668fa
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-28 16:23:56 -0600

    Added the Billing Component

---
+++ shard-mock-up/src/components/Billing.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Billing = (props) => {
+  return (
+    <div>
+      <div className="row">
+
+        <div className="col-4 bodyLeft">
+          <p className="mt-4"> Billing </p>
+          <p className="text"> Setup Your Billing Data and Payment Details </p>
+        </div>
+
+        <div className="col-8 bodyRight">
+          <div className="rightContent">
+            <div className="row ml-2">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <hr className="hrUnits" />
+    </div>
+  )
+}
+
+export default Billing
--- shard-mock-up/src/components/MainBody.js
+++ shard-mock-up/src/components/MainBody.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Reports from './Reports'
 import SearchAds from './SearchAds'
+import Billing from './Billing'
 
 class MainBody extends Component {
   constructor(props) {
@@ -116,6 +117,7 @@ class MainBody extends Component {
           enableActive3={this.enableActive3}
           open={this.state.open}
         />
+        <Billing />
 
       </div >
     )
