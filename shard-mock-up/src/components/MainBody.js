71ca23d1679aa2542d16bdd712e96cfe82870306
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-27 18:11:32 -0600

    Converted the mainBody into a class component so it could have initial  state

--- shard-mock-up/src/components/MainBody.js
+++ shard-mock-up/src/components/MainBody.js
@@ -1,49 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Reports from './Reports'
 import SearchAds from './SearchAds'
 
-const MainBody = (props) => {
-  return (
-    <div className="container-fluid mainBody">
-      <div className="row">
+class MainBody extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false,
+    }
+  }
 
-        <div className="col-4 bodyLeft">
-          <p className="mt-4"> System </p>
-          <p className="text">Configure external fees and email that will be used as default address for notifications</p>
-          <div> <a href="/" rel="noopener noreferrer">How does Apple commission influence my statistic?</a> </div>
-          <div> <a href="/" rel="noopener noreferrer">How do attribution tracking expenses mean?</a> </div>
-        </div>
+  render() {
+    return (
+      <div className="container-fluid mainBody" >
+        <div className="row">
 
-        <div className="col-8 bodyRight">
-          <div className="rightContent">
-            <div className="title my-2"><p> General Settings </p></div>
-            <hr className="hrBoxes" />
-
-            <div className="row">
-              <div>
-                <div className="textBox mb-4"> Email </div>
-                <div className="textBox my-4"> Commission Charges </div>
-                <div className="textBox"> Tracking Expense </div>
-              </div>
-              <div className="ml-4 rightSetting">
-                <div className="textBox mb-4"> <input className="inputStyle1" placeholder="gregory.murynmukha@gmail.com" /> </div>
-                <div className="textBox my-4"><input className="inputStyle" type="checkbox" />  Take into account Apple commision charges (30%) </div>
-                <div className="textBox"> <input className="inputStyle" placeholder="$0.00" /> per each install </div>
-              </div>
-            </div>
-
-            <hr className="hrBoxes" />
-            <button className="btn btn-primary mr-1 floatButtonRight" href="/" disabled> Save Changes </button>
+          <div className="col-4 bodyLeft">
+            <p className="mt-4"> System </p>
+            <p className="text">Configure external fees and email that will be used as default address for notifications</p>
+            <div> <a href="/" rel="noopener noreferrer">How does Apple commission influence my statistic?</a> </div>
+            <div> <a href="/" rel="noopener noreferrer">How do attribution tracking expenses mean?</a> </div>
           </div>
+
+          <div className="col-8 bodyRight">
+            <div className="rightContent">
+              <div className="title my-2"><p> General Settings </p></div>
+              <hr className="hrBoxes" />
+
+              <div className="row">
+                <div>
+                  <div className="textBox mb-4"> Email </div>
+                  <div className="textBox my-4"> Commission Charges </div>
+                  <div className="textBox"> Tracking Expense </div>
+                </div>
+                <div className="ml-4 rightSetting">
+                  <div className="textBox mb-4"> <input className="inputStyle1" placeholder="gregory.murynmukha@gmail.com" /> </div>
+                  <div className="textBox my-4"><input className="inputStyle" type="checkbox" />  Take into account Apple commision charges (30%) </div>
+                  <div className="textBox"> <input className="inputStyle" placeholder="$0.00" /> per each install </div>
+                </div>
+              </div>
+
+              <hr className="hrBoxes" />
+              <button className="btn btn-primary mr-1 floatButtonRight" href="/" disabled> Save Changes </button>
+            </div>
+          </div>
+
         </div>
+        <hr className="hrUnits" />
+        <Reports />
+        <SearchAds />
 
-      </div>
-      <hr className="hrUnits" />
-      <Reports />
-      <SearchAds />
-
-    </div >
-  )
+      </div >
+    )
+  }
 }
+
 
 export default MainBody
--- shard-mock-up/src/components/SearchAds.js
+++ shard-mock-up/src/components/SearchAds.js
@@ -13,23 +13,36 @@ const SearchAds = () => {
         <div className="col-8 bodyRight">
           <div className="rightContent">
 
-            <div className="row">
-              <i className="fas fa-circle fa-sm my-4"></i>
-              <div className="textBox my-4"> Joshua Moody </div>
-              <button className="btn btn-primary" href="/"> New Button </button>
+            <div className="row py-1">
+              <i className="fas fa-circle fa-sm lightIcon"></i>
+              <div>
+                <div className="textBox"> Joshua Moody </div>
+                <div className="lightText">Added May 21st 2017</div>
+              </div>
+              <button className="btn btn-primary floatButtonRight" href="/"> Enable </button>
             </div>
-            <div className="textBox">Added May 21st 2017</div>
 
-            <div className="row">
-              <i className="fas fa-circle fa-sm my-4"></i>
-              <div className="textBox my-4"> Bernard Rhodes </div>
-              <button className="btn btn-primary" href="/"> New Button </button>
+            <div className="row py-1">
+              <i className="fas fa-circle fa-sm lightIcon"></i>
+              <div>
+                <div className="textBox"> Bernard Rhodes </div>
+                <div className="lightText"> Added June 10th 2017 </div>
+              </div>
+              <button className="btn btn-primary floatButtonRight" href="/"> Enable </button>
             </div>
-            <div className="textBox">Added June 10th 2017</div>
 
-            <div className="row">
-              <i className="fas fa-cloud-upload-alt fa-lg my-4"></i>
-              <div className="textBox my-4"> Drop archive with API certificate here or choose <a href="/">choose file</a> to add new account </div>
+            <div className="row py-1">
+              <i className="fas fa-circle fa-sm lightIcon"></i>
+              <div>
+                <div className="textBox"> Eugene mcCarthy </div>
+                <div className="lightText"> Added Aug 12th, 2017 </div>
+              </div>
+              <button className="btn btn-primary floatButtonRight" href="/"> Disable </button>
+            </div>
+
+            <div className="row py-1">
+              <i className="fas fa-cloud-upload-alt fa-lg darkerGray"></i>
+              <div className="text ml-3"> Drop archive with API certificate here or choose <a href="/">choose file</a> to add new account </div>
             </div>
 
           </div>
