a76abef1e35ac8d5b62b93f7273df3e6c8c156c7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-29 16:02:42 -0600

    Added the Billing right section content

--- shard-mock-up/src/components/Billing.js
+++ shard-mock-up/src/components/Billing.js
@@ -12,9 +12,33 @@ const Billing = (props) => {
 
         <div className="col-8 bodyRight">
           <div className="rightContent">
-            <div className="row ml-2">
+            <div className="title my-2 ml-2"><p> Billing Information </p></div>
+            <hr className="hrBoxes" />
 
+            <div className="row">
+              <div className="flexColumn ml-2">
+                <div className="textBox"> Email </div>
+                <div className="textBox my-4"> Commission Charges </div>
+                <div className="textBox"> Tracking Expense </div>
+              </div>
+              <div className="ml-2 flexColumn">
+                <div className="textBox"> <input className="pl-2 inputStyle1" placeholder="gregory.murynmukha@gmail.com" /> </div>
+                <div className="textBox my-4"> <input className="mr-1 inputStyle" 
+                // onClick={this.checkedSetting} 
+                type="checkbox" />  Take into account Apple commision charges (30%) </div>
+                <div className="textBox"> <input className=" inputStyle" placeholder="$0.00" /> per each install </div>
+              </div>
             </div>
+
+            <hr className="hrBoxes" />
+
+            {/* {this.state.checked
+              ? <button className="btn btn-primary mr-1 floatButtonRight" href="/"> Save Changes </button>
+              : <button className="btn btn-primary mr-1 floatButtonRight" href="/" disabled> Save Changes </button>
+            } */}
+
+            <button className="btn btn-primary mr-1 floatButtonRight" href="/" disabled> Save Changes </button>
+
           </div>
         </div>
 
