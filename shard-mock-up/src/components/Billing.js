1bfe9a102a17032b6213925d4707ec82ccebfbf2
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-06-29 18:33:05 -0600

    Created a grayButton class

--- shard-mock-up/src/components/Billing.js
+++ shard-mock-up/src/components/Billing.js
@@ -11,7 +11,9 @@ const Billing = (props) => {
         </div>
 
         <div className="col-8 bodyRight">
-          <div className="rightContent">
+          <div className="rightContentChunk">
+
+            <div className="rightContent pb-5">
             <div className="row spacer negMargin">
               <div className="title ml-2"><p> Billing Information </p></div>
               <div className="title"><a href="/"><p> View Billing History </p> </a></div>
@@ -50,12 +52,37 @@ const Billing = (props) => {
             <hr className="hrBoxes" />
 
             {props.inputChange
-              ? <button className="btn btn-primary mr-1 floatButtonRight" href="/"> Save Changes </button>
-              : <button className="btn btn-primary mr-1 floatButtonRight" href="/" disabled> Save Changes </button>
+              ? <button className="btn btn-primary mr-1 floatButtonRight negMar" href="/"> Save Changes </button>
+              : <button className="btn btn-primary mr-1 floatButtonRight negMar" href="/" disabled> Save Changes </button>
             }
+            </div>
+
+            
+            <div className="row mt-3 py-3 spacer rightContent">
+              <div className="row ml-2">
+                {props.active2
+                  ? <i className="fas fa-circle fa-sm active"></i>
+                  : <i className="fas fa-circle fa-sm lightIcon"></i>
+                }
+                <div className="ml-3">
+                  <div className="textBox"> Mastercard Ending in 4482 </div>
+                  <div className="lightText"> 07/20 </div>
+                </div>
+              </div>
+              <div>
+                <button className="btn  floatButtonRight grayButton"
+                  // onClick={props.enableActive2} 
+                  href="/">
+                  Edit Card
+                </button>
+              </div>
+            </div>
+         
 
           </div>
         </div>
+
+        
 
       </div>
       <hr className="hrUnits" />
